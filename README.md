3a8a7e8149d7f61f8fdf5a8ee672418b2da2d1c6
Andrew Santhosh <andrewsanthosh264@gmail.com>
2024-12-06 11:30:10 +0530

    use gpu acceleration if available

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # hf transformers gradio chat ui
+```commandline
+pip install -r requirements.txt
+```
+```commandline
+python main.py
+```
--- main.py
+++ main.py
@@ -1,14 +1,21 @@
+import torch
 import gradio as gr
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 
-model_name = "HuggingFaceTB/SmolLM2-135M-Instruct"
-device = "cpu"
+model_name = "HuggingFaceTB/SmolLM2-1.7B-Instruct"
+device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Model selected: {model_name}")
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(model_name).to(device)
 
-SYSTEM_PROMPT = """Your name is Sydney, You are a AI model made by Andrew. You are a helpful assistant.
-"""
+if device == "cuda": quantization_config = BitsAndBytesConfig(load_in_8bit=True)
+else: quantization_config = None
+
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name, quantization_config=quantization_config)
+
+SYSTEM_PROMPT = """
+Your name is Sydney, You are a AI model made by Andrew. You are a helpful assistant.
+- You use a lot of emojis when chatting.
+""".strip()
 
 
 def run_inference(message: str, history: list):
@@ -17,7 +24,7 @@ def run_inference(message: str, history: list):
     history.append(chat_message)
     input_text = tokenizer.apply_chat_template(history, tokenize=False)
     inputs = tokenizer.encode(input_text, return_tensors="pt").to(device)
-    outputs = model.generate(inputs, max_new_tokens=256, temperature=0.2, top_p=0.9, do_sample=True)
+    outputs = model.generate(inputs, max_new_tokens=256, temperature=1.5, top_p=0.9, do_sample=True)
     decoded = tokenizer.decode(outputs[0])
     response = decoded.split("<|im_start|>assistant\n")[-1].split("<|im_end|>")[0]
 
